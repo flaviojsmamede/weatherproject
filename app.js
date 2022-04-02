839e55d9512396ab33591c2bafd0fae75823b210
Flávio <85414870+flaviojsmamede@users.noreply.github.com>
2022-04-02 12:04:19 +0100

    GET live information from the API

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Lisbon&appid=9f2c2ac34d0c1e53035fdc5b448eaa26&units=metric"
+
+  https.get(url, function(response) {
+    console.log(response.statusCode);
+
+    response.on("data", function(data){
+      const weatherData = JSON.parse(data)
+      const temp = weatherData.main.temp
+      const weatherDescription = weatherData.weather[0].description
+      const icon = weatherData.weather[0].icon
+      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+      res.write("<h1>The temperature in Lisbon is " + temp + " degrees Celcius.</h1>");
+      res.write("<p>The weather is currently " + weatherDescription + "</p>");
+      res.write("<img src=" + imageURL + ">");
+      res.send();
+    })
+  });
+})
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000.");
+})
